f7049c78a355434112d77fc723d359ae995a7ef6
lnlnta <146244787+lnlnta@users.noreply.github.com>
2024-04-02 11:08:29 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+//Si, todo el código lo he hecho yo, que pasa.
+
+//ChangeLanguage
+var but_changelanguage = document.getElementById("but_changelanguage")
+
+but_changelanguage.addEventListener("click", changelanguage);
+//
+
+//FunctionChangeLanguage
+function changelanguage(){
+  alert("Esto se encuentra en desarrollo...\nThis is currently in development...")
+}
